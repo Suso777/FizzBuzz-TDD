04260852d8361337588dd354ef1debbc26a94f62
Suso777 <xesussuarez@gmail.com>
2026-03-09 13:20:30 +0100

    Fix change FizzBuzzTest.java

--- src/main/java/org/example/Fizzbuzz.java
+++ src/main/java/org/example/Fizzbuzz.java
@@ -3,6 +3,31 @@ package org.example;
 public class Fizzbuzz {
     public String convert(int inputNumber){
 
+        //     if(inputNumber == 7){
+        //         return "7";
+        //  }
+
+        //     if(inputNumber == 4){
+        //      return "4";
+        //      }
+        //      if(inputNumber == 2){
+        //         return "2";
+        //      }
+
+        //      return "1";
+
+//        if(inputNumber == 3){
+//            return "Fizz";
+//        }
+//
+//        if(inputNumber == 6){
+//            return "Fizz";
+//        }
+//
+//        if(inputNumber == 9){
+//            return "Fizz";
+//        }
+
         if(inputNumber % 3 == 0 && inputNumber % 5 == 0){
             return "FizzBuzz";
         }
