e5120c3b20f66e08ee53eedef25fe34bfacb242e
Suso777 <xesussuarez@gmail.com>
2026-03-09 13:29:31 +0100

    Feat FizzBuzzTest.java

--- src/test/java/FizzBuzzTest/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest/FizzBuzzTest.java
@@ -1,4 +1,8 @@
-package FizzBuzzTest;
+package Fizzbuzz;
+
+import org.example.Fizzbuzz;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
-}
